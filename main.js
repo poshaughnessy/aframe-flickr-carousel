c292bb90c9bcbaf46e55e3a07b5b71ce506a6c05
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2016-09-12 00:07:42 +0100

    Got images showing up

---
+++ main.js
@@ -0,0 +1,25 @@
+
+// Arrange in a sphere - borrowing this from: http://threejs.org/examples/css3d_periodictable.html
+
+/*
+var vector = new THREE.Vector3();
+
+for ( var i = 0, l = objects.length; i < l; i ++ ) {
+
+  var phi = Math.acos( -1 + ( 2 * i ) / l );
+  var theta = Math.sqrt( l * Math.PI ) * phi;
+
+  var object = new THREE.Object3D();
+
+  object.position.x = 800 * Math.cos( theta ) * Math.sin( phi );
+  object.position.y = 800 * Math.sin( theta ) * Math.sin( phi );
+  object.position.z = 800 * Math.cos( phi );
+
+  vector.copy( object.position ).multiplyScalar( 2 );
+
+  object.lookAt( vector );
+
+  targets.sphere.push( object );
+
+}
+*/
