016b00bd8667c8b26e3de06ee33ffb36ddcb53a9
Peter O'Shaughnessy <peter.oshaughnessy@gmail.com>
2016-09-12 11:49:26 +0100

    Attempting circular layout, but images are not rotated

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 // To generate an API URL, visit: https://www.flickr.com/services/api/explore/flickr.photos.search
-var API_URL = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=caa934e3ee0864ccdd36a5e671ab5563&tags=llama&per_page=10&format=json&nojsoncallback=1&api_sig=05bc4e460ee7a5602d96989cc5d1b898';
+var API_URL = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=d02d27104d6cb6d8d9124917feedf869&tags=llama&extras=o_dims&per_page=10&format=json&nojsoncallback=1';
 
 var scene = document.getElementById('scene');
 var assets = document.getElementById('assets');
+var imageContainer = document.getElementById('imageContainer');
 
 var testEntity = document.getElementById('test-entity');
 
@@ -12,21 +13,21 @@ function generateImage(id, src) {
 
   var imgEl = document.createElement('img');
   imgEl.id = 'img' + id;
+  imgEl.crossOrigin = "anonymous";
   imgEl.setAttribute('src', src);
+  //imgEl.setAttribute('src', 'images/llama-512.jpg');
 
   assets.appendChild(imgEl);
 
   var aframeImageEl = document.createElement('a-image');
   aframeImageEl.setAttribute('src', '#img' + id);
+  aframeImageEl.setAttribute('width', 1.25);
   aframeImageEl.setAttribute('height', 1);
-  aframeImageEl.setAttribute('width', 1);
 
-  var entityEl = document.createElement('a-entity');
-  entityEl.setAttribute('position', {x: id - 5, y: 1, z: -3});
+  var entity = document.createElement('a-entity');
+  entity.appendChild(aframeImageEl);
 
-  entityEl.appendChild(aframeImageEl);
-
-  scene.appendChild(entityEl);
+  imageContainer.appendChild(entity);
 
 }
 
@@ -57,18 +58,42 @@ function generateImages() {
 
 }
 
-fetch(API_URL)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(json) {
-    console.log('JSON response', json);
-    processUrls(json.photos.photo);
-    generateImages();
-  })
-  .catch(function (err) {
-    console.error('Unable to fetch photos', err);
+function fetchImages() {
+
+  fetch(API_URL)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      console.log('JSON response', json);
+      processUrls(json.photos.photo);
+      generateImages();
+    })
+    .catch(function (err) {
+      console.error('Unable to fetch photos', err);
+    });
+
+}
+
+function onAssetsLoaded() {
+
+  console.log('Assets loaded!');
+
+}
+
+function init() {
+
+  assets.addEventListener('loaded', onAssetsLoaded);
+
+  scene.addEventListener('loaded', function() {
+    console.log('Scene loaded');
   });
+
+  fetchImages();
+
+}
+
+init();
 
 
 // TODO: Arrange in a sphere - borrowing this from: http://threejs.org/examples/css3d_periodictable.html
