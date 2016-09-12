0103664dc71c45da617fc41ac03e15262bf1be9c
Peter O'Shaughnessy <peter.oshaughnessy@gmail.com>
2016-09-12 14:03:05 +0100

    Attempting animation and tidying

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 // To generate an API URL, visit: https://www.flickr.com/services/api/explore/flickr.photos.search
-var API_URL = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=d02d27104d6cb6d8d9124917feedf869&tags=llama&extras=o_dims&per_page=10&format=json&nojsoncallback=1';
+var API_URL = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=d02d27104d6cb6d8d9124917feedf869&tags=llama&per_page=9&format=json&nojsoncallback=1';
+
+var CAROUSEL_ROTATE_Y_CHANGE = 360 / 9;
 
 var scene = document.getElementById('scene');
 var assets = document.getElementById('assets');
 var imageContainer = document.getElementById('imageContainer');
+var carouselAnimation = document.getElementById('carouselAnimation');
 
-var testEntity = document.getElementById('test-entity');
-
+var currentRotationY = 10;
 var imageSrcArray = [];
+
 
 function generateImage(id, src) {
 
@@ -25,6 +28,8 @@ function generateImage(id, src) {
 
   var entity = document.createElement('a-entity');
   entity.appendChild(aframeImageEl);
+
+  // Rotate the images to face the user in the centre
   entity.setAttribute('look-at', '0 0.75 0');
 
   imageContainer.appendChild(entity);
@@ -38,13 +43,6 @@ function generateImages() {
   }
 
   console.log('Generated images');
-
-}
-
-/**
- * The aframe-layout-component positions the images, but does not rotate them for us.
- */
-function rotateImages() {
 
 }
 
@@ -65,6 +63,15 @@ function processUrls(photos) {
 
 }
 
+function updateAnimation() {
+
+  currentRotationY += CAROUSEL_ROTATE_Y_CHANGE;
+
+  carouselAnimation.setAttribute('from', '0 ' + currentRotationY + ' 0');
+  carouselAnimation.setAttribute('to', '0 ' + (currentRotationY + CAROUSEL_ROTATE_Y_CHANGE) + ' 0');
+
+}
+
 function fetchImages() {
 
   fetch(API_URL)
@@ -75,7 +82,6 @@ function fetchImages() {
       console.log('JSON response', json);
       processUrls(json.photos.photo);
       generateImages();
-      rotateImages();
     })
     .catch(function (err) {
       console.error('Unable to fetch photos', err);
@@ -83,48 +89,9 @@ function fetchImages() {
 
 }
 
-function onAssetsLoaded() {
-
-  console.log('Assets loaded!');
-
-}
-
 function init() {
-
-  assets.addEventListener('loaded', onAssetsLoaded);
-
-  scene.addEventListener('loaded', function() {
-    console.log('Scene loaded');
-  });
-
+  carouselAnimation.addEventListener('animationend', updateAnimation);
   fetchImages();
-
 }
 
 init();
-
-
-// TODO: Arrange in a sphere - borrowing this from: http://threejs.org/examples/css3d_periodictable.html
-
-/*
-var vector = new THREE.Vector3();
-
-for ( var i = 0, l = objects.length; i < l; i ++ ) {
-
-  var phi = Math.acos( -1 + ( 2 * i ) / l );
-  var theta = Math.sqrt( l * Math.PI ) * phi;
-
-  var object = new THREE.Object3D();
-
-  object.position.x = 800 * Math.cos( theta ) * Math.sin( phi );
-  object.position.y = 800 * Math.sin( theta ) * Math.sin( phi );
-  object.position.z = 800 * Math.cos( phi );
-
-  vector.copy( object.position ).multiplyScalar( 2 );
-
-  object.lookAt( vector );
-
-  targets.sphere.push( object );
-
-}
-*/
