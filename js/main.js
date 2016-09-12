29eafbeb20fdaec8c35c5929ad0a3f564ba325ea
Peter O'Shaughnessy <peter.oshaughnessy@gmail.com>
2016-09-12 15:19:54 +0100

    Tidying

--- js/main.js
+++ js/main.js
@@ -18,7 +18,6 @@ var imageSrcArray = [];
 function gearVRHeightFix() {
   if (AFRAME.utils.isGearVR()) {
     cameraContainer.setAttribute('position', '0 1.6 0');
-    alert('Updated camera height');
   }
 }
 
